8d8335a419c31b6c7c991afe39aca7cd7d3b280d
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2023-01-31 09:41:57 +0100

    Add files via upload

---
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,25 @@
+package co.develhope.mockdb.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
